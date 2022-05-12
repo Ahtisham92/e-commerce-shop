739164fce6453233c59da96bec18bd2b824d2ed4
Ahtisham Ul Haq <ahtisham388@outlook.com>
2022-05-12 15:36:39 +0500

    added custom links and test to header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,14 +4,14 @@ import { Container, Navbar, Nav } from 'react-bootstrap'
 const Header = () => {
   return (
     <header>
-      <Navbar bg="light" expand="lg">
+      <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="/">ProShop</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
+              <Nav.Link href="/cart">Cart</Nav.Link>
+              <Nav.Link href="/login">Sign In</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
