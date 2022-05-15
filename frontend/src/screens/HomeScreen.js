63cc770f6b09e13ffc63574aab0e5dfcdf50c1c6
Ahtisham Ul Haq <ahtisham388@outlook.com>
2022-05-15 11:36:02 +0500

    added products

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import products from '../products'
+import {Col, Row} from 'react-bootstrap'
 
 const HomeScreen = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <h3>Products list</h3>
+      <Row>
+        {
+          products.map(product => {
+            return <Col>
+              <h2>{product.name}</h2>
+              <p>{product.description}</p>
+            </Col>
+          })  
+        }
+      </Row> 
+    </>
   )
 }
 
