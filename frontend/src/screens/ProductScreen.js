0d42624f38eef4f81007315cc6fe6519a06feb11
Ahtisham Ul Haq <ahtisham388@outlook.com>
2022-05-23 00:24:13 +0500

    added product screen component

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -16,11 +16,11 @@ const ProductScreen = () => {
       <Link to='/' className='btn btn-light my-3'>Go Back</Link>
       <Row>
         <Col md={6}>
-          <Image src={product.image} alt={product.name} />
+          <Image src={product.image} alt={product.name} fluid />
         </Col>
 
         <Col md={3}>
-          <ListGroup>
+          <ListGroup variant='flush'>
             <ListGroup.Item>
               <h3>{product.name}</h3>
             </ListGroup.Item>
@@ -40,12 +40,36 @@ const ProductScreen = () => {
               {product.description}
             </ListGroup.Item>
 
+
           </ListGroup>
         </Col>
 
         <Col md={3}>
           <Card>
             <ListGroup variant='flush'>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <span style={{fontWeight: 'bold'}}>${product.price}</span>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    {product.countInStock > 0? 'In Stock': 'Out Of Stock'}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                <Button className='btn-block' disabled={product.countInStock <= 0}>
+                  Add to Cart
+                </Button>
+              </ListGroup.Item>
 
             </ListGroup>
           </Card>
