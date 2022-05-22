133f4485952ad1958cb05e4abb8bd06787bae72f
Ahtisham Ul Haq <ahtisham388@outlook.com>
2022-05-22 15:32:12 +0500

    imported some stuff

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+
 
 const ProductScreen = () => {
   return (
-    <div>ProductScreen</div>
+    <div></div>
   )
 }
 
