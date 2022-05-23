8b9e91c46999e414a94e4ac9c1255c537d8f0f13
Ahtisham Ul Haq <ahtisham388@outlook.com>
2022-05-23 18:06:28 +0500

    added products from the backedn

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import products from '../products'
 import {Col, Row} from 'react-bootstrap'
 import Product from '../components/Product'
-
+import axios from 'axios'
 
 const HomeScreen = () => {
+
+  const [products, setProducts] = useState([])
+
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products')
+      console.log(data)
+      setProducts(data);
+    }
+
+    fetchProducts();
+
+  }, []);
+
   return (
     <>
       <h1>Products list</h1>
