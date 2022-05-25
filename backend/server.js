bb060087ac25f0706d4faa346ab2cc90c36219d1
Ahtisham Ul Haq <ahtisham388@outlook.com>
2022-05-25 15:25:35 +0500

    added Node_env and Port in th server

--- backend/server.js
+++ backend/server.js
@@ -23,4 +23,6 @@ app.get('/api/products/:id', (req, res) => {
 })
 
 
-app.listen(5000, console.log('Server is running on port  5000'))
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`))
