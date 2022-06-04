d590fe48698a75e2a66cab5bd8249283b88cb536
Ahtisham Ul Haq <ahtisham388@outlook.com>
2022-06-04 23:11:15 +0500

    added asynchandler

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,18 +5,24 @@ import asyncHandler from 'express-async-handler'
 const router = express.Router()
 
 
-route.get('/', (req, res) => {
+route.get('/',  asyncHandler(async (req, res) => {
   const product = await Product.find({})
 
   res.json(products)
-})
+}))
 
 
 
-route.get('/:id', (req, res) => {
-  const product = products.find(p => p._id === req.params.id)
-  res.json(product)
-})
+route.get('/:id', asyncHandler( async (req, res) => {
+  const product = await Product.find(p => p._id === req.params.id)
+  
+  if(product) {
+    res.json(product)
+  } else {
+    res.status(404).json({ message: 'Product not found' })
+  }
+
+}))
 
 
 
