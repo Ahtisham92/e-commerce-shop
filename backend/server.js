85ec9c0cea4ad545f6f861f5c10567dd053abe67
Ahtisham Ul Haq <ahtisham388@outlook.com>
2022-06-04 22:59:15 +0500

    importe Product

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
+import Product from  './models/productModel.js'
 
 
 dotenv.config();
