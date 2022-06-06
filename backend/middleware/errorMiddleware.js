c8d74548263792d1f81d3d6ff09428e520d1b62d
Ahtisham Ul Haq <ahtisham388@outlook.com>
2022-06-06 16:37:02 +0500

    imported the function to server.js and corrected the export in EMW.js

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -17,4 +17,4 @@ const errorHandler = (err, req, res, next) => {
   })
 }
 
-export default {notFound, errorHandler}
+export {notFound, errorHandler}
--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
 import  colors from 'colors'
-
+import { notFound , errorHandler } from './middleware/errorMiddleware.js'
 
 dotenv.config();
 
@@ -21,9 +21,9 @@ app.get('/', (req, res) => {
 app.use('/api/products/', productRoutes)
 
 
-app.use()
+app.use(notFound)
 
-app.use()
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
